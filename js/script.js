767dae53d18b78ef98dbb9d6371a74f4e97d8c3e
Rogier van der Weerd <rogerscode@gmail.com>
2024-12-27 17:43:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -160,28 +160,3 @@ function filterAlbums() {
         }
     });
 }
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    const form = event.target;
-    const formData = new FormData(form);
-
-    // Replace 'your-formspree-endpoint' with your actual Formspree endpoint
-    fetch('https://formspree.io/f/xkggjlyo', {
-        method: 'POST',
-        body: formData,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            document.getElementById('formResponse').textContent = 'Thank you for your message!';
-            form.reset();
-        } else {
-            document.getElementById('formResponse').textContent = 'Oops! There was a problem submitting your form.';
-        }
-    }).catch(error => {
-        document.getElementById('formResponse').textContent = 'Oops! There was a problem submitting your form.';
-    });
-});
